7e0e105d7f9e6150a9861b9796ef15bc2211467d
yushuaisan <shuaisan@buffalo.edu>
2018-10-05 16:32:08 -0400

    Update todo.js

--- src/redux/actions/todo.js
+++ src/redux/actions/todo.js
@@ -30,19 +30,9 @@ const delTodo = (todoId) => ({
 
 /* default 导出所有 Action Creators */
 export default {
-  // 虽然是同步的函数，但请不要自行 bindActionCreators
-  // 皆因调用 connect 后，react-redux 已经帮我们做了，见：
-  // https://github.com/reactjs/react-redux/blob/master/src/utils/wrapActionCreators.js
   addTodo, toggleTodo, delTodo
 }
 
-// ================================
-// Action handlers for Reducer
-// 本来更新 state 是 Reducer 的责任
-// 但要把 ActionType 导出又引入实在太麻烦
-// 且在 Reducer 中写 switch-case 实在太不优雅
-// 故在此直接给出处理逻辑
-// ================================
 export const ACTION_HANDLERS = {
   [ADD_TODO]: (todos, { payload }) => [ ...todos, payload ],
   [TOGGLE_TODO]: (todos, { payload: todoId }) => todos.map(
