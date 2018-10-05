4d87aa9a8b29d850d245f92084a782fdf149c052
yushuaisan <shuaisan@buffalo.edu>
2018-10-05 16:32:27 -0400

    Update user.js

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -50,13 +50,6 @@ export default {
   login, checkLogin, logout
 }
 
-// ================================
-// Action handlers for Reducer
-// 本来更新 state 是 Reducer 的责任
-// 但要把 ActionType 导出又引入实在太麻烦
-// 且在 Reducer 中写 switch-case 实在太不优雅
-// 故在此直接给出处理逻辑
-// ================================
 export const ACTION_HANDLERS = {
   [LOG_IN]: (userData, { payload }) => payload, // payload is userData
   [LOG_OUT]: () => null
